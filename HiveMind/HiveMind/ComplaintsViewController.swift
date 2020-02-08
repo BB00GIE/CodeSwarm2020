81a500f34d5756634b5d9ce95f5afe4116e5a76e
Boogie <bmelmore18@students.desu.edu>
2020-02-08 15:08:02 -0500

    FInal Submission

--- HiveMind/HiveMind/ComplaintsViewController.swift
+++ HiveMind/HiveMind/ComplaintsViewController.swift
@@ -14,39 +14,37 @@ class ComplaintsViewController: UIViewController, UITableViewDataSource, UITable
     var i = 0
     let db = Firestore.firestore()
     
+    
     @IBOutlet weak var ComplaintLabel: UILabel!
     @IBOutlet var tableView: UITableView!
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        let dataList = ["one","two","three","four","five"]
         return dataList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ComplaintCell", for: indexPath) as! ComplaintCell
-        var c = "apple"
-        let dataList = ["one","two","three","four","five"]
+//        var c = "apple"
         
         let docRef = db.collection("mrasamny@desu.edu").document(dataList[i])
         
-        docRef.getDocument { (document, error) in
-            if let document = document, document.exists {
-                
-                
-                let dataDescription = document.data()
-                
-                let CurrentComp = dataDescription as! [String: String]
-                
-                let hi  =  CurrentComp["complaintList"]!
-                
-                c = hi
-            } else {
-                //print("Document does not exist")
-            }
-        }
-        cell.hello.text = c as! String
+//        docRef.getDocument { (document, error) in
+//            if let document = document, document.exists {
+//
+//                let dataDescription = document.data()
+//
+//                let CurrentComp = dataDescription as! [String: String]
+//
+//                let hi  =  CurrentComp["complaintList"]!
+//
+//                c = hi
+//            } else {
+//                //print("Document does not exist")
+//            }
+//        }
         
+        cell.hello.text = dataList[indexPath.row]
         
         return cell
         
@@ -58,6 +56,8 @@ class ComplaintsViewController: UIViewController, UITableViewDataSource, UITable
         
         tableView.dataSource = self
         tableView.delegate = self
+        tableView.reloadData()
+    
         
         // Do any additional setup after loading the view.
     }
---
+++ HiveMind/HiveMind/data.swift
@@ -0,0 +1,11 @@
+//
+//  data.swift
+//  HiveMind
+//
+//  Created by Brandon Elmore on 2/8/20.
+//  Copyright © 2020 CodeSwarm. All rights reserved.
+//
+
+import Foundation
+
+let dataList = ["Dr.Rasamny is the greatest","I love the new lounge","I wish we still had couches","There is a shortage of outlets in the department","The students don't take care of the lounge"]
